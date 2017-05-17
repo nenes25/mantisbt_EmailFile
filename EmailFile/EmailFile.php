2b11b8f49b9c8aeca62a5c814a2ad514efd8d6fd
herve <nenes_fr@yahoo.fr>
2017-05-17 21:41:42 +0200

    compatibily mantis bt 2.2 and sup

--- EmailFile/EmailFile.php
+++ EmailFile/EmailFile.php
@@ -31,9 +31,9 @@ class EmailFilePlugin extends MantisPlugin {
         $this->name = lang_get('plugin_emailfile_title');
         $this->description = lang_get('plugin_emailfile_description');
         $this->page = 'config.php';
-        $this->version = '0.1.2';
+        $this->version = '0.1.4';
         $this->requires = array(
-            'MantisCore' => '1.3.0',
+            'MantisCore' => '2.2',
         );
         $this->author = 'Hennes Hervé';
         $this->url = 'http://www.h-hennes.fr';
--- EmailFile/pages/config.php
+++ EmailFile/pages/config.php
@@ -12,7 +12,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with MantisBT.  If not, see <http://www.gnu.org/licenses/>.
-
 #
 #  EmailFile Plugin for Mantis BugTracker :
 #  - Add attachments to bug with an email
@@ -23,47 +22,81 @@
 auth_reauthenticate();
 access_ensure_global_level(config_get('manage_plugin_threshold'));
 
-html_page_top(plugin_lang_get('title'));
+layout_page_header(plugin_lang_get('title'));
 
-print_manage_menu();
+layout_page_begin('manage_overview_page.php');
+
+print_manage_menu('manage_plugin_page.php');
 ?>
 <br />
 <p><?php echo plugin_lang_get('process_description'); ?></p>
 
-<div style="border:1px solid #000;margin-top:20px;margin-bottom:20px;padding:10px;">
-    <p><?php echo plugin_lang_get('launch_reception_script');?> : <a href="<?php echo plugin_page('get_emails'); ?>" target="_blank"><?php echo plugin_lang_get('script_name');?></a></p>
+<div class="alert alert-info">
+    <p><?php echo plugin_lang_get('launch_reception_script'); ?> : <a href="<?php echo plugin_page('get_emails'); ?>" target="_blank"><?php echo plugin_lang_get('script_name'); ?></a></p>
 </div>
 
 <form action="<?php echo plugin_page('config_edit') ?>" method="post">
-    <table>
-        <tr <?php echo helper_alternate_class() ?>>
-            <td class="category"><?php echo plugin_lang_get('config_email_host'); ?></td>
-            <td><input type="text" name="email_host" size="50" value="<?php echo plugin_config_get('email_host'); ?>" /></td>
-        </tr>
-        <tr <?php echo helper_alternate_class() ?>>
-            <td class="category"><?php echo plugin_lang_get('config_email_address'); ?></td>
-            <td><input type="text" name="email_address" size="50" value="<?php echo plugin_config_get('email_address'); ?>" /></td>
-        </tr>
-        <tr <?php echo helper_alternate_class() ?>>
-            <td class="category"><?php echo plugin_lang_get('config_email_password'); ?></td>
-            <td><input type="password" name="email_password" size="50"value="<?php echo plugin_config_get('email_password'); ?>" /></td>
-        </tr>
-        <tr <?php echo helper_alternate_class() ?>>
-            <td class="category"><?php echo plugin_lang_get('config_email_fetch_mode'); ?>*</td>
-            <td>
-                <select name="email_fetch_mode">
-                    <option value="manual" <?php if ( plugin_config_get('email_fetch_mode') == 'manual' ) :?> selected="selected"<?php endif; ?>><?php echo plugin_lang_get('config_email_fetch_mode_manual');?></option>
-                    <option value="ajax" <?php if ( plugin_config_get('email_fetch_mode') == 'ajax' ) :?> selected="selected"<?php endif; ?>><?php echo plugin_lang_get('config_email_fetch_mode_ajax');?></option>
-                    <option value="cron" <?php if ( plugin_config_get('email_fetch_mode') == 'cron' ) :?> selected="selected"<?php endif; ?>><?php echo plugin_lang_get('config_email_fetch_mode_cron');?></option>
-                </select>
-            </td>
-        </tr>
-        <tr <?php echo helper_alternate_class() ?>>
-            <td class="center" colspan="2"><input type="submit" value="<?php echo plugin_lang_get("config_action_update") ?>"/></td>
-        </tr>
-    </table>
-</form>
+    <div class="widget-box widget-color-blue2">
+        <div class="widget-header widget-header-small">
+            <h4 class="widget-title lighter">
+                <i class="ace-icon fa fa-edit"></i>
+                <?php echo plugin_lang_get('edit_email'); ?>
+            </h4>
+        </div>
+        <div class="widget-body">
+            <div class="widget-main no-padding">
+                <table class="table table-bordered table-condensed table-striped">
+                    <tr <?php echo helper_alternate_class() ?>>
+                        <th class="category">
+                            <label for="email_host"><?php echo plugin_lang_get('config_email_host'); ?></label>
+                        </th>
+                        <td>
+                            <input type="text" name="email_host" size="50" value="<?php echo plugin_config_get('email_host'); ?>" class="input-sm"/>
+                        </td>
+                    </tr>
+                    <tr <?php echo helper_alternate_class() ?>>
+                        <th class="category">
+                            <label for="email_address"><?php echo plugin_lang_get('config_email_address'); ?></label>
+                        </th>
+                        <td>
+                            <input type="text" name="email_address" size="50" value="<?php echo plugin_config_get('email_address'); ?>" class="input-sm"/>
+                        </td>
+                    </tr>
+                    <tr <?php echo helper_alternate_class() ?>>
+                        <th class="category">
+                            <label for="email_password"><?php echo plugin_lang_get('config_email_password'); ?></label>
+                        </th>
+                        <td>
+                            <input type="password" name="email_password" size="50"value="<?php echo plugin_config_get('email_password'); ?>" class="input-sm"/>
+                        </td>
+                    </tr>
+                    <tr <?php echo helper_alternate_class() ?>>
+                        <th class="category">
+                            <label for="email_fetch_mode"><?php echo plugin_lang_get('config_email_fetch_mode'); ?>*</label>
+                        </th>
+                        <td>
+                            <select name="email_fetch_mode" class="input-sm">
+                                <option value="manual" <?php if (plugin_config_get('email_fetch_mode')
+                    == 'manual') :
+                    ?> selected="selected"<?php endif; ?>><?php echo plugin_lang_get('config_email_fetch_mode_manual'); ?></option>
+                                <option value="ajax" <?php if (plugin_config_get('email_fetch_mode')
+                                        == 'ajax') :
+                                        ?> selected="selected"<?php endif; ?>><?php echo plugin_lang_get('config_email_fetch_mode_ajax'); ?></option>
+                                <option value="cron" <?php if (plugin_config_get('email_fetch_mode')
+                                            == 'cron') :
+                                            ?> selected="selected"<?php endif; ?>><?php echo plugin_lang_get('config_email_fetch_mode_cron'); ?></option>
+                            </select>
+                        </td>
+                    </tr>
+                </table>
 
+            </div>
+        </div>
+        <div class="widget-toolbox padding-8 clearfix">
+            <input type="submit" class="btn btn-primary btn-white btn-round" value="<?php echo plugin_lang_get("config_action_update") ?>"/>
+        </div>
+    </div>
+</form>
 <div class="hint" style="margin-top:20px;">
     *<?php echo plugin_lang_get('config_email_fetch_mode_description'); ?>
 </div>
